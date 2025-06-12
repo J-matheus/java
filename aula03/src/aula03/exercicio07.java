541a67551a276db727439cf496036ef5cbef57c6
Jose matheus <j.matheus.v.souza@gmail.com>
2025-06-12 11:04:21 -0300

    soma de 3, 5 e total

--- aula03/src/aula03/exercicio07.java
+++ aula03/src/aula03/exercicio07.java
@@ -4,9 +4,17 @@ public class exercicio07 {
 	public static void main(String[] args) {
 		Scanner sete = new Scanner(System.in);
 		System.out.println("Informe a quantidade de alunos: ");
-		int resp = sete.nextInt();
+		int qtd = sete.nextInt();
+		double soma=0;
+		for (int x=0; x<qtd ; x++) {
+			System.out.println(x);
+			
+			System.out.println("Informe a nota: ");
+			soma+=sete.nextDouble();
+		}
+		double media = soma/qtd;
+		System.out.println(media);
 		
-		for()
 	}
 
 }
